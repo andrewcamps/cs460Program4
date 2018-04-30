0b749ffc4d059c9befe647ef8785d0d581dc310f
Andrew Robert Camps <andrewcamps@cs.arizona.edu>
2018-04-30 04:57:29 -0700

    Added simple delete student page

--- src/main/java/program4/MainController.java
+++ src/main/java/program4/MainController.java
@@ -20,7 +20,7 @@ import java.sql.*;
 @Controller
 public class MainController {
 
-	@Autowired
+		@Autowired
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
@@ -29,45 +29,38 @@ public class MainController {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        String sql = "insert into people values (?)";
-        jdbcTemplate.update(sql, name);
-        return "greeting";
-    }
-
-@GetMapping("/addPerson")
+		@GetMapping("/addPerson")
     public String addPerson(){
         return "addPerson";        
     }
 
-
-   @GetMapping("/addPerson/postStudent")
+   	@GetMapping("/addPerson/postStudent")
     public String postStudent(@RequestParam int StudentID, @RequestParam String Name, @RequestParam String Address, @RequestParam String Phone, @RequestParam String Email, @RequestParam String Gender, @RequestParam String DOB, @RequestParam String Category, @RequestParam String Major, @RequestParam String Minor, @RequestParam int AdvisorID, Model model) {
         String sql = "insert into Student values (?,?,?,?,?,?,?,?,?,?,?)";
-	java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+				java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
         jdbcTemplate.update(sql, StudentID, Name, Address, Phone, Email, Gender,sqlDate, Category, Major, Minor, AdvisorID);
         return "addPerson";
     }
-@GetMapping("/addPerson/postStaff")
+		
+		@GetMapping("/addPerson/postStaff")
     public String postStaff(@RequestParam int StaffID, @RequestParam String Name, @RequestParam String Email, @RequestParam String Address, @RequestParam String DOB, @RequestParam String Gender, @RequestParam String Title, @RequestParam String Location, Model model) {
         String sql = "insert into Staff values (?,?,?,?,?,?,?,?)";
-	java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+				java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
         jdbcTemplate.update(sql,StaffID, Name, Email, Address, sqlDate, Gender, Title, Location);
         return "addPerson";
     }
 
-@GetMapping("/addLease")
+		@GetMapping("/addLease")
     public String addLease(){
         return "addLease";        
     }
-@GetMapping("/updateRent")
+		
+		@GetMapping("/updateRent")
     public String updateRent(){
         return "updateRent";        
     }
-@GetMapping("/deleteStudent")
+		
+		@GetMapping("/deleteStudent")
     public String deleteStudent(){
         return "deleteStudent";        
     }
