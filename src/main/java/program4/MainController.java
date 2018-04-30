cf2613365706355e5d0c90b07ef16dcd96f55170
GarrettSeale <seale.garrett@gmail.com>
2018-04-29 20:04:45 -0700

    New staff and students are posting to DB

--- src/main/java/program4/MainController.java
+++ src/main/java/program4/MainController.java
@@ -10,10 +10,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.sql.*;
 
 @Controller
 public class MainController {
@@ -40,6 +42,23 @@ public class MainController {
     public String addPerson(){
         return "addPerson";        
     }
+
+
+   @GetMapping("/addPerson/postStudent")
+    public String postStudent(@RequestParam int StudentID, @RequestParam String Name, @RequestParam String Address, @RequestParam String Phone, @RequestParam String Email, @RequestParam String Gender, @RequestParam String DOB, @RequestParam String Category, @RequestParam String Major, @RequestParam String Minor, @RequestParam int AdvisorID, Model model) {
+        String sql = "insert into Student values (?,?,?,?,?,?,?,?,?,?,?)";
+	java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+        jdbcTemplate.update(sql, StudentID, Name, Address, Phone, Email, Gender,sqlDate, Category, Major, Minor, AdvisorID);
+        return "addPerson";
+    }
+@GetMapping("/addPerson/postStaff")
+    public String postStaff(@RequestParam int StaffID, @RequestParam String Name, @RequestParam String Email, @RequestParam String Address, @RequestParam String DOB, @RequestParam String Gender, @RequestParam String Title, @RequestParam String Location, Model model) {
+        String sql = "insert into Staff values (?,?,?,?,?,?,?,?)";
+	java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+        jdbcTemplate.update(sql,StaffID, Name, Email, Address, sqlDate, Gender, Title, Location);
+        return "addPerson";
+    }
+
 @GetMapping("/addLease")
     public String addLease(){
         return "addLease";        
