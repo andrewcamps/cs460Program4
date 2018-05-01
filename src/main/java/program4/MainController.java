085dd6254de0a343675e9331b1fbdd2dc9f57b6e
andrewcamps <andrewcamps@email.arizona.edu>
2018-04-30 20:10:38 -0700

    test query

--- src/main/java/program4/MainController.java
+++ src/main/java/program4/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
@@ -15,7 +16,7 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 import java.sql.*;
 
 @Controller
